dd1c3d29b325c78edf9e086d9f45ebdae0f5e833
ColdThunder11 <lslyj27761@gmail.com>
2020-03-19 22:03:40 +0800

    1.0

---
+++ AdnmbBackup-gui/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace AdnmbBackup_gui
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(132, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(198, 28);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "串号（id）";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(368, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "请在同一个目录下新建cookie.txt将饼干放入";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(143, 118);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(116, 18);
+            this.linkLabel1.TabIndex = 4;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "如何拿到饼干";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(331, 110);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 34);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "开始备份！";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Location = new System.Drawing.Point(28, 118);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(80, 18);
+            this.linkLabel2.TabIndex = 6;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "项目地址";
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(351, 42);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 18);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "无任务";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(494, 177);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "A岛匿名版备份器";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ AdnmbBackup-gui/Form1.cs
@@ -0,0 +1,160 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+using System.Net;
+using System.Net.Http;
+
+namespace AdnmbBackup_gui
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.coldthunder11.com/artical/%e6%95%99%e7%a8%8b/%e5%a6%82%e4%bd%95%e8%8e%b7%e5%8f%96a%e5%b2%9b%e7%9a%84%e9%a5%bc%e5%b9%b2/");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string id = textBox1.Text;
+            if (id == string.Empty) return;
+            if (!File.Exists("cookie.txt"))
+            {
+                MessageBox.Show("请先放好小饼干");
+                return;
+            }
+            if (!Directory.Exists(DateTime.Now.Month.ToString() + "-" + DateTime.Now.Day.ToString()))
+                Directory.CreateDirectory(DateTime.Now.Month.ToString() + "-" + DateTime.Now.Day.ToString());
+            string path = Path.Combine(DateTime.Now.Month.ToString() + "-" + DateTime.Now.Day.ToString(), id + ".json");
+            try
+            {
+                string url = "https://nmb.fastmirror.org/Api/thread";
+                var cookie = File.ReadAllText("cookie.txt");
+                CookieContainer cookieContainer = new CookieContainer();
+                cookieContainer.Add(new Cookie("userhash", cookie, "/", "nmb.fastmirror.org"));
+                HttpClientHandler handler = new HttpClientHandler() { UseCookies = true };
+                handler.CookieContainer = cookieContainer;
+                HttpClient http = new HttpClient(handler);
+                http.DefaultRequestHeaders.Add("Host", "nmb.fastmirror.org");
+                http.DefaultRequestHeaders.Add("Accept", "application/json");
+                http.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36 HavfunClient-AdnmbBackup");
+                label4.Text = "正在获取第1页";
+                var t = http.GetAsync(url + "?id=" + id + "&page=1");
+                t.Wait();
+                var result = t.Result;
+                var t2 = result.Content.ReadAsByteArrayAsync();
+                t2.Wait();
+                var bytes = t2.Result;
+                var str = ReadGzip(bytes);
+                var fpjson = JsonConvert.DeserializeObject<JObject>(str);
+                var replyCount = int.Parse(fpjson["replyCount"].ToString());
+                int pageCount = replyCount / 19;
+                if (replyCount / pageCount != 0) pageCount++;
+                JArray contentJA = fpjson["replys"].ToObject<JArray>();
+                for (var page = 2; page <= pageCount; page++)
+                {
+                    label4.Text = "正在获取第" + page + "页";
+                    t = http.GetAsync(url + "?id=" + id + "&page=" + page);
+                    t.Wait();
+                    result = t.Result;
+                    t2 = result.Content.ReadAsByteArrayAsync();
+                    t2.Wait();
+                    bytes = t2.Result;
+                    str = ReadGzip(bytes);
+                    var jo = JsonConvert.DeserializeObject<JObject>(str);
+                    JArray ja = jo["replys"].ToObject<JArray>();
+                    var rpcount = ja.Count;
+                    for (int j = 0; j < rpcount; j++)
+                    {
+                        contentJA.Add(ja[j]);
+                    }
+                }
+                for (var index = 0; index < contentJA.Count; index++)
+                {
+                    if (contentJA[index]["title"].ToString() == "广告")
+                    {
+                        contentJA.RemoveAt(index);
+                        index--;
+                    }
+                }
+                fpjson["replys"].Replace(contentJA);
+                var fjsonstr = JsonConvert.SerializeObject(fpjson, Formatting.Indented);
+                File.WriteAllText(path, fjsonstr);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                label4.Text = "啊哦，出错了";
+                return;
+            }
+            ConvertToText(path);
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/ColdThunder11/AdnmbBackup-gui");
+        }
+        static void ConvertToText(string path)
+        {
+            var jo = JsonConvert.DeserializeObject<JObject>(File.ReadAllText(path));
+            var sb = new StringBuilder();
+            sb.Append(jo["userid"].ToString()); sb.Append("  "); sb.Append(jo["now"].ToString());
+            sb.Append("  No."); sb.Append(jo["id"].ToString()); sb.Append(Environment.NewLine);
+            if (jo["title"].ToString() != "无标题")
+            {
+                sb.Append("标题:"); sb.Append(jo["title"].ToString()); sb.Append(Environment.NewLine);
+            }
+            sb.Append(ContentProcess(jo["content"].ToString())); sb.Append(Environment.NewLine);
+            var ja = jo["replys"].ToObject<JArray>();
+            for (int i = 0; i < ja.Count; i++)
+            {
+                sb.Append("------------------------------"); sb.Append(Environment.NewLine);
+                sb.Append(ja[i]["userid"].ToString()); sb.Append("  "); sb.Append(ja[i]["now"].ToString());
+                sb.Append("  No."); sb.Append(ja[i]["id"].ToString()); sb.Append(Environment.NewLine);
+                sb.Append(ContentProcess(ja[i]["content"].ToString())); sb.Append(Environment.NewLine);
+            }
+            File.WriteAllText(path.Replace("json", "txt"), sb.ToString());
+            var lines = File.ReadAllLines(path.Replace("json", "txt"));
+            for (var i = 0; i < lines.Length; i++)
+            {
+                lines[i] = lines[i].Trim();
+            }
+            File.WriteAllLines(path.Replace("json", "txt"), lines);
+        }
+        static string ContentProcess(string content)
+        {
+            return content.Replace("<font color=\"#789922\">&gt;&gt;", ">>").Replace("</font><br />", Environment.NewLine)
+                .Replace("</font>", Environment.NewLine)
+                .Replace("<br />\r\n", Environment.NewLine).Replace("<br />\n", Environment.NewLine);
+        }
+        static string ReadGzip(byte[] bytes)
+        {
+            string result = string.Empty;
+            using (MemoryStream ms = new MemoryStream(bytes))
+            {
+                using (GZipStream decompressedStream = new GZipStream(ms, CompressionMode.Decompress))
+                {
+                    using (StreamReader sr = new StreamReader(decompressedStream, Encoding.UTF8))
+                    {
+                        result = sr.ReadToEnd();
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
