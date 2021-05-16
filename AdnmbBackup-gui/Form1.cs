22054ffb5b8641924bff62902f3b1eb352dbbda0
gchengyu <86chengyujin@gmail.com>
2021-05-16 09:40:35 +0800

    Update Form1.cs

--- AdnmbBackup-gui/Form1.cs
+++ AdnmbBackup-gui/Form1.cs
@@ -129,7 +129,7 @@ namespace AdnmbBackup_gui
                 sb.Append("  No."); sb.Append(ja[i]["id"].ToString()); sb.Append(Environment.NewLine);
                 sb.Append(ContentProcess(ja[i]["content"].ToString())); sb.Append(Environment.NewLine);
             }
-            File.WriteAllText(path.Replace("json", "txt"), sb.ToString(), Encoding.GetEncoding("gb2312"));
+            File.WriteAllText(path.Replace("json", "txt"), sb.ToString(), Encoding.GetEncoding("GBK"));
         }
         static void ConvertToTextPoOnly(string path)
         {
@@ -154,7 +154,7 @@ namespace AdnmbBackup_gui
                     sb.Append(ContentProcess(ja[i]["content"].ToString())); sb.Append(Environment.NewLine);
                 }
             }
-            File.WriteAllText(path.Replace(".json", "_po_only.txt"), sb.ToString(), Encoding.GetEncoding("gb2312"));
+            File.WriteAllText(path.Replace(".json", "_po_only.txt"), sb.ToString(), Encoding.GetEncoding("GBK"));
         }
         static string ContentProcess(string content)
         {
